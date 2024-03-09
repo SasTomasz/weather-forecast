1b8f57e2a298b2d9b91e29fb9d855c64d8809b6f
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-03-09 10:29:36 +0100

    Add skeleton of convert to csv func

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import pandas as pd
 import streamlit as st
 
-import weather_data_receiver
-
-
-def prepare_data_for_chart(period, area):
-    # TODO: Resolve problem with load DataFrame
-    data = weather_data_receiver.get_demand_data(period, area)
-    df = pd.read_json(data)
-    print(df)
-    pass
-
+from weather_data_operations import prepare_data_for_chart
 
 if __name__ == '__main__':
     st.title("Weather Forecast for the Next Days")
--- weather_data_operations.py
+++ weather_data_operations.py
@@ -1,12 +1,18 @@
+import csv
+import json
+from http import HTTPStatus
+
+import pandas as pd
 import requests
 import streamlit as st
-from http import HTTPStatus
+
 from models import WeatherData, Hour
-import json
+
+
 # This project get data from https://www.visualcrossing.com/
 
 
-def get_data_from_api(period, area="Kraków"):
+def get_data_from_api(area="Kraków"):
     key = st.secrets["API_KEY"]
     base_url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{area}?unitGroup=metric&include=days%2Chours&key={key}&contentType=json"
     r = requests.get(base_url)
@@ -30,16 +36,37 @@ def transform_data(data):
 
 
 def get_demand_data(period, area):
-    data = get_data_from_api(period, area)
+    data = get_data_from_api(area)
     if data.status_code == HTTPStatus.OK:
         data = data.json()
         demand_data = data["days"][:period]
+        # print(f"DEMAND DATA = \n{demand_data}")
         demand_data = transform_data(demand_data)
     else:
         return "There is no data available"
     return demand_data
 
 
+def prepare_data_for_chart(period, area):
+    # TODO: Resolve problem with load DataFrame
+    data = get_demand_data(period, area)
+    df = pd.read_json(data)
+    print(df)
+
+
+def convert_json_to_csv(json_data):
+    json_data = json.loads(json_data)
+    data_file = open("./data/weather_data.csv", "w")
+    csv_writer = csv.writer(data_file)
+    for i in json_data:
+        print(i["datetime"])
+        csv_writer.writerows(i)
+    data_file.close()
+
+
 if __name__ == "__main__":
     user_demand_data = get_demand_data(5, "Kraków")
     print(user_demand_data)
+    # convert_json_to_csv(user_demand_data)
+    # print(data_for_chart)
+    # prepare_data_for_chart(5, "Kraków")
